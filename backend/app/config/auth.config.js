51731e5d3fb247c83aa7cd5b7728b1660457c7ce
AshishYsoft <ashishpysoft@gmail.com>
2022-06-17 11:19:00 +0530

    backend

--- backend/app/config/auth.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    secret: "secret-key",
-    user: "kamyasysoft@gmail.com", 
-    pass: "ysoft@123", 
-    jwtExpiration: 36000,       
-    jwtRefreshExpiration: 36000, 
-  };
